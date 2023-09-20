ece134631f0798b00b78ae586cabd7274e37a186
abhayprasad565 <113719175+abhayprasad565@users.noreply.github.com>
2023-09-20 07:48:23 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,179 +1,123 @@
-// define property to change heading
-let title = document.querySelector("title");
-var link = document.querySelector("link[rel*='icon']");
-document.addEventListener('visibilitychange', function (event) {
-    if (document.hidden) {
+const Apikey = "652b1808fa4c482ca569229d952f0d0a";
+let page = 1;
+const currdate = new Date();
+let day = currdate.getDate();
+let month = currdate.getMonth() + 1;
+let year = currdate.getFullYear();
+let date = `${year}-${month}-${day}`;
+// variables 
+let query = undefined;
+let category = undefined;
+let currPage="";
 
+let nextP = document.querySelector("#nextPage");
 
-        // Create a new favicon URL
-        title.textContent = "Hey Come Back !!";
-        var newFaviconUrl = "img/pray.png";
+// next page and previous page buttons
+nextP.addEventListener("click",()=> 
+{  
+        page+=1;
+       currPage() ;
+        
+});
+let prevP = document.querySelector("#prevPage");
+prevP.addEventListener("click",()=> {
+    if(page>=2)
+    {
+        page-=1;
+    currPage();
 
-        // Set the 'href' attribute of the <link> element to the new favicon URL
-        link.href = newFaviconUrl;
-    } else {
-        title.textContent = "Portfolio | Abhay Kumar";
-        var newFaviconUrl = "img/Abhay-bitmoji.png";
-        link.href = newFaviconUrl;
     }
+});
+console.log(page);
+// categories button
+let science = document.querySelector("#science");
+science.addEventListener("click",function()
+{    category="science";
+     query=undefined;
+     page=1;
+    fetchTopHeadlines();
+});
+let sports = document.querySelector("#sports");
+
+sports.addEventListener("click",function()
+{
+    category="sports";
+    query=undefined;
+    page=1;
+    fetchTopHeadlines();
+})
+// search bar
+const search = document.querySelector("#search");
+search.addEventListener("click",function(event)
+{ 
+     let input  = document.querySelector("#input");
+     category=undefined;
+     query=input.value;
+    console.log(query);
+    page = 1;
+   fetchSearch();
+    
 });
 
 
 
+ const fetchTopHeadlines = async ()=>
+ {   
+     currPage=fetchTopHeadlines;
+    let url = "https://newsapi.org/v2/top-headlines?";
+    if(query!=undefined) url+=`q=${query}&`;
+   url+="country=in&"+
+    "language=en&"+
+    "from="+date+"&to=2023-05-01&"+
+    "PageSize=20&";
+    if(page!=undefined)
+    url+= "page="+page+"&";
+   else url+="page=1&"
+    if(category!=undefined)url=url+"category="+category+"&";
+    url+="apiKey="+Apikey;
+   console.log(url);
+        printRes(url);  
+ };
+ const fetchSearch = ()=>
+ {   currPage=fetchSearch;
+    let url = "https://newsapi.org/v2/everything?" ;
+    if(query!=undefined)
+      url+= "q="+query+"&";
+    else url+="q=india&";
+   // +"country=in&"
+   url+= "language=en&"+
+    "from="+date+"&to=2023-06-16&"+
+    //"sortBy=popularity&"+
+    "PageSize=20&"+
+    "page="+page+"&"+
+    "apiKey="+Apikey;
+   console.log(url);
+   printRes(url);
+ }
 
 
-
-
-
-// define on scroll parralax
-let text = document.querySelector("#parallax-text");
-let mountain1 = document.querySelector("#m1");
-let tree2 = document.querySelector("#t2");
-let mountain2 = document.querySelector("#m2");
-let tree1 = document.querySelector("#t1");
-let man = document.querySelector("#man");
-let plants = document.querySelector("#plants");
-let introCard = document.querySelector(".intro-card");
-let skillscard = document.querySelector(".skills-container");
-
-window.addEventListener("scroll", () => {   // select the value scrolled 
-
-    // parallax animation
-    let value = window.scrollY;
-
-
-
-
-
-    // animation for  intro card
-    let top = window.scrollY;
-
-
-
-    parallaxAnimate(top);
-    introCardAnimate(top);
-    skillcardAnimate(top);
-    educationCardAnimate(top);
-
-
-});
-
-function parallaxAnimate(value) {
-    parallaxY = ((screen.width) / 100);
-    if (screen.width > 900) parallaxY *= 5;
-    else parallaxY *= 80;
-    text.style.marginTop = value * 1.5 + 'px';
-    text.style.opacity = (100) - value * 0.5 + '%';
-
-    mountain1.style.top = (parallaxY) + value * (-0.1) + 'px';
-    mountain2.style.top = (parallaxY) + value * (-0.1) + 'px';
-
-
-    plants.style.bottom = value * -0.01 + 'px';
-    tree2.style.left = (-100) + value * +0.2 + 'px';
-    tree2.style.bottom = value * -0.1 + 'px';
-    man.style.left = (-250) + value * (0.4) + 'px';
-}
-
-// animation for skills card
-function skillcardAnimate(top) {
-
-    let skillsOffSet = skillscard.offsetTop - 500;
-    let skillsHeight = skillscard.offsetHeight;
-    if (top >= skillsOffSet && top < skillsOffSet + skillsHeight || ((top < skillsOffSet + skillsHeight + 500 && top > skillsOffSet))) {
-
-        if (!skillscard.classList.contains("show-animate")) {
-            console.log("skills added class");
-            skillscard.classList.add("show-animate");
-        }
-
-    }
-    else if (top > skillsOffSet + skillsHeight + 500 || top < skillsOffSet) {
-        if (skillscard.classList.contains("show-animate")) {
-            console.log("skills remove class");
-            skillscard.classList.remove("show-animate");
-        }
-    }
-}
-
-// animation for intro card
-function introCardAnimate(top) {
-    let offset = introCard.offsetTop - 500;
-    let height = introCard.offsetHeight;
-    if (top >= offset && top < offset + height || (top < offset + height + 500 && top > offset)) {
-
-        if (!introCard.classList.contains("show-animate")) {
-            console.log("added class");
-            introCard.classList.add("show-animate");
-        }
-
-    }
-    else if (top > offset + height + 500 || top < offset) {
-        if (introCard.classList.contains("show-animate")) {
-            console.log("removed class");
-            introCard.classList.remove("show-animate");
-        }
-    }
-}
-
-
-// animate the education section
-let edsection = document.querySelector(".education");
-let edcards = document.querySelectorAll(".education-card");
-function educationCardAnimate(top) {
-    let offset = edsection.offsetTop - 500;
-    let height = edsection.offsetHeight;
-    if (top >= offset && top < offset + height || (top < offset + height + 500 && top > offset)) {
-
-        if (!edcards[0].classList.contains("show-animate")) {
-            console.log("added education class");
-            for (card of edcards)
-                card.classList.add("show-animate");
-        }
-
-    }
-    else if (top > offset + height + 500 || top < offset) {
-        if (edcards[0].classList.contains("show-animate")) {
-            console.log("ed card removed");
-            for (card of edcards)
-                card.classList.remove("show-animate");
-        }
-    }
-}
-
-
-
-
-
-
-let projects = document.querySelectorAll(".project-card");
-for (card of projects) {
-    card.addEventListener("click", function () {
-        console.log(window.location.href = this.getAttribute("link"));
-    })
-}
-
-function sendMail() {
-    let params = {
-        name: document.querySelector("#name").value,
-        email: document.querySelector("#emails").value,
-        phone: document.querySelector("#phone").value,
-        message: document.querySelector("#message").value + "sent by " + document.querySelector("#emails").value
-    }
-    const serviceId = "service_tjjusg9";
-    emailjs.send(serviceId, "template_71acm9t", params)
-        .then(
-            res => {
-
-                alert("your message was sent");
-            }
-        ).catch((err) => console.log(err));
-}
-
-
-
-
-
+async function printRes(url)
+ {
+           let req = new Request(url);
+         let a = await fetch(req);
+         let response = await a.json();
+   // create a temp response to not exhaust api
+    let card = `<h2>Top news (${response.totalResults})</h2>`;
+      for(let item of response.articles)
+      {
+        card = card+`<div class="card my-4 mx-2 " style="width: 18rem;">
+        <img src="${item.urlToImage}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">+${item.title}+</h5>
+          <p class="card-text">${item.description}</p>
+          <a href="${item.url}" target="_blank" class="btn btn-primary">Read Full Article</a>
+        </div>
+      </div>`
+      }
+     document.querySelector(".container").innerHTML=card;
+ }
+ fetchTopHeadlines();
+ 
 
 
 
